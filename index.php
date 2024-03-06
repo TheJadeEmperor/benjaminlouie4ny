2cc52f361b20a138f5081f1bc0f9e8610b7aa292
TheJadeEmperor <TheJadeEmperor@users.noreply.github.com>
2024-03-06 00:52:22 -0500

    platform content

--- index.php
+++ index.php
@@ -48,40 +48,57 @@ include('menu.php');
           <div class="section-heading">
             <h2>Platforms</h2>
           </div>
+
+          <p>Democrats have destroyed New York close to the point of no return. New Yorkers are leaving in droves and migrants are coming in by the thousands. I have heard the voices of concerned citizens and I will be your voice, the voice of the silent majority. </p>
         </div>
-      
+        <div class="row">
           <div class="col-4">
             <img src="assets/images/ga_patrol_1.jpg" width="100%" />
           </div>
           <div class="col-8">
-          <h2>Public Safety</h2>
+            <h2>Public Safety</h2>
 
-<p>According to the 62nd Precinct, rape is up 50% and carjacking is up 16%
-Everyday citizens are worried about assaults, robberies, package theft, and even domestic violence
-The police have their hands tied with all these restrictive rules. I will fight for more police funding and more police presence on the streets and subways
-As a former Guardian Angel, I patrolled the subways often and understand the fears many New Yorkers face when riding the subway
-I will fight to end bail reform to keep criminals off our streets</p>
- 
-<h2>Migrating the Migrants</h2>
+            <p>The Democrats are trying to convince you that crime is down city wide, but according to the 62nd Precinct, rape is up 50% and carjacking is up 16%. Everyday citizens are worried about assaults, robberies, package theft, and even domestic violence. The police have their hands tied with all these restrictive rules. I will fight for more police funding and more police presence on the streets and subways.<p>
+            <p>As a former Guardian Angel, I patrolled the subways often and understand the fears many New Yorkers face when riding the subway.</p>
 
-<p>Migrants are receiving free credit cards and healthcare at the expense of our taxpayers
-I will fight to stop funding the migrants and instead use those funds for services needed by American citizens. Funding should go to senior’s healthcare services, Medicaid and long term care
-I will fight to stop turning our schools into migrant shelters. Our children’s education should be our first priority.
-These funds should be used on our veterans and homeless people</p>
+            <p>Most New Yorkers do not feel safe. If you drive, you can get carjacked. If you take the subway, you can get attacked. If you walk, you can get attacked by illegal mopeds. NYC is returning to the way it was in the 80s and 90s. That is why I will fight to end bail reform to keep criminals off our streets.</p>
+            
+            <h2>Migrating the Migrants</h2>
 
- 
+            <p>Migrants are receiving free credit cards and healthcare at the expense of our taxpayers
+            I will fight to stop funding the migrants and instead use those funds for services needed by American citizens. Funding should go to senior’s healthcare services, Medicaid and long term care
+            I will fight to stop turning our schools into migrant shelters. Our children’s education should be our first priority.
+            These funds should be used on our veterans and homeless people</p>
 
-<h2>Education</h2>
+            
 
-<p>Our education and specialized schools need to return to a meritocracy system, not a diversity based system
-We need smaller class sizes so our students get more attention to teachers. Currently, migrants are added to classes, increasing class size and students don’t get enough attention
-</p>
-
-<h2>Other</h2>
-
-<p>I will fight to undo Good Cause Eviction. Our homeowners and landlords are already dealing with difficult tenant laws, and this new law will have homeowners tied behind their backs.
-I am strictly pro-life. I believe unborn babies should have rights as a person.   </p>   
+          
           </div>
+
+          <p>&nbsp;</p>
+
+          <div class="row">
+            <div class="col-8">
+              <h2>Education</h2>
+
+              <p>Our education and specialized schools need to return to a meritocracy system, not a diversity based system.
+              We need smaller class sizes so our students get more attention to teachers. Currently, migrants are added to classes, increasing class size and students don’t get enough attention
+              </p>
+              <h2>Other</h2>
+
+              <p>I will fight to undo Good Cause Eviction. Our homeowners and landlords are already dealing with difficult tenant laws, and this new law will have homeowners tied behind their backs.
+              I am strictly pro-life. I think it's crazy that migrants are treated as citizens while unborn babies are not. </p>   
+
+
+            </div>
+            <div class="col-4">
+              <img src="assets/images/headshot_1.jpg" width="100%" />
+
+            </div>
+
+          </div>
+
+        </div>
         </div>
       </div>
     </div>
