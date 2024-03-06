99f7bf71e09bd76ab0f54010130c4a6c0c1b4b78
TheJadeEmperor <TheJadeEmperor@users.noreply.github.com>
2024-03-06 00:31:05 -0500

    delete useless stuff

--- index.php
+++ index.php
@@ -13,9 +13,9 @@ include('menu.php');
 
         <li><a href="#platform">Platform</a></li>
 
-        <li><a href="#why_us">Volunteer</a></li>
+        <li><a href="#volunteer">Volunteer</a></li>
        
-        <li><a href="resume.php" class="external">Donate</a></li>
+        <li><a href="#" class="external">Donate</a></li>
  
       </ul>
     </nav>
@@ -49,112 +49,59 @@ include('menu.php');
             <h2>Platforms</h2>
           </div>
         </div>
-        <div class="col-md-12">
-
-          <div class="col-md-6">
-            <img src="assets/images/ga_patrol_1.jpg" />
+      
+          <div class="col-4">
+            <img src="assets/images/ga_patrol_1.jpg" width="100%" />
           </div>
-          <div class="col-md-6">
-            
+          <div class="col-8">
+          <h2>Public Safety</h2>
+
+<p>According to the 62nd Precinct, rape is up 50% and carjacking is up 16%
+Everyday citizens are worried about assaults, robberies, package theft, and even domestic violence
+The police have their hands tied with all these restrictive rules. I will fight for more police funding and more police presence on the streets and subways
+As a former Guardian Angel, I patrolled the subways often and understand the fears many New Yorkers face when riding the subway
+I will fight to end bail reform to keep criminals off our streets</p>
+ 
+<h2>Migrating the Migrants</h2>
+
+<p>Migrants are receiving free credit cards and healthcare at the expense of our taxpayers
+I will fight to stop funding the migrants and instead use those funds for services needed by American citizens. Funding should go to senior’s healthcare services, Medicaid and long term care
+I will fight to stop turning our schools into migrant shelters. Our children’s education should be our first priority.
+These funds should be used on our veterans and homeless people</p>
+
+ 
+
+<h2>Education</h2>
+
+<p>Our education and specialized schools need to return to a meritocracy system, not a diversity based system
+We need smaller class sizes so our students get more attention to teachers. Currently, migrants are added to classes, increasing class size and students don’t get enough attention
+</p>
+
+<h2>Other</h2>
+
+<p>I will fight to undo Good Cause Eviction. Our homeowners and landlords are already dealing with difficult tenant laws, and this new law will have homeowners tied behind their backs.
+I am strictly pro-life. I believe unborn babies should have rights as a person.   </p>   
           </div>
         </div>
       </div>
     </div>
   </section>
 
-  <section class="section courses" data-section="port_personal">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="section-heading">
-            <h2>Personal Portfolio</h2>
-          </div>
-        </div>
-
-        <div class="owl-carousel owl-theme">
-
-        <?php
-        foreach($carousel_personal as $h4 => $val) {
-          echo '<div class="item">
-            <a href="'.$val['img'].'" class="external" target="_BLANK"><img src="'.$val['img'].'" alt="'.$h4.'"></a>
-            <div class="down-content">
-            <h4>'.$h4.'</h4>
-            <p>'.$val['p'].'</p>';
-
-            echo '
-          </div>
-          </div>';
-        }
-        ?>
-         
-        </div>
-      </div>
-    </div>
-  </section>
-  
-
-  <section class="section courses" data-section="">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="section-heading">
-            <h2>Professional Portfolio</h2>
-          </div>
-        </div>
-
-        <div class="owl-carousel owl-theme">
-
-        <?php
-        foreach($carousel_pro as $h4 => $val) {
-          echo '<div class="item">
-            <a href="'.$val['img'].'" class="external" target="_BLANK"><img src="'.$val['img'].'" alt="'.$h4.'"></a>
-            <div class="down-content">
-            <h4>'.$h4.'</h4>
-            <p>'.$val['p'].'</p>';
-
-            echo '
-          </div>
-          </div>';
-        }
-        ?>
-         
-        </div>
-      </div>
-    </div>
-  </section>
-  
-  <section class="section courses" data-section="port_client">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="section-heading">
-            <h2>Client Portfolio</h2>
-          </div>
-        </div>
-        <div class="owl-carousel owl-theme">
-
-        <?php
-        foreach($carousel_client as $h4 => $val) {
-          echo '<div class="item">
-            <a href="'.$val['img'].'" class="external" target="_BLANK"><img src="'.$val['img'].'" alt="'.$h4.'"></a>
-            <div class="down-content">
-            <h4>'.$h4.'</h4>
-            <p>'.$val['p'].'</p>';
-            
-            if($val['github']) {
-              echo '<p><a href="'.$val['github'].'" class="external" target="_BLANK">Github</a></p>';
-            }
-
-            echo '
-          </div>
-          </div>';
-        }
-        ?>
    
+  <section class="section courses" data-section="volunteer">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-heading">
+            <h2>Volunteer</h2>
+          </div>
         </div>
+
+       
       </div>
     </div>
   </section>
+   
 
 <?php 
 include('footer.html');
